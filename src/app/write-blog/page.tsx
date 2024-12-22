65d397deb901ac724c6ab8423aa1a318b1cb05c7
SaurabhKute <saurabhkute321@gmail.com>
2024-12-22 15:15:33 +0530

    added useeffect fix

--- src/app/write-blog/page.tsx
+++ src/app/write-blog/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, {  useRef, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { EditorState, convertToRaw } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
