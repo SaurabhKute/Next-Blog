1ee219b4fd0d441ffbbe9efae63b1892f0f96bf2
SaurabhKute <saurabhkute321@gmail.com>
2025-02-05 16:42:27 +0530

    added seo changes

--- src/app/manage-posts/page.tsx
+++ src/app/manage-posts/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Manage Posts - The Daily Scribble",
+    description: "A blog platform built with Next.js",
+    keywords: ["posts, blogs", "manage"]
+  };
+
+
 export default function Page() {
     return (
         <p>Manage Posts</p>
--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Statistics - The Daily Scribble",
+    description: "A blog platform built with Next.js",
+    keywords: ["statistics, graphs"]
+  };
+
 export default function Page() {
     return (
         <p>Statistics</p>
